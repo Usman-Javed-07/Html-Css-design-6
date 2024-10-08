75e5fd01bdb07a99e7e2e4953a90bb5421219e0c
Usman Javed <usmanjaved0816@gmail.com>
2024-10-08 15:33:21 +0500

    work on add to card

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ const pageName = pathName.split("/").pop();
 if(pageName === "index.html") {
   document.querySelector (".home").classList.add("activeLink");
 }
+
+//  add to card
+
+
+let removeCardItemButton = document.getElementsByClassName ('remove-Btn');
+console.log(removeCardItemButton);
+for (let i = 0; i < removeCardItemButton.length ; i++) {
+  let button = removeCardItemButton[i]
+  button.addEventListener ('click', function (){
+    let buttonclicked = event.target;
+    buttonclicked.parentElement.parentElement.remove();
+  });
+}
