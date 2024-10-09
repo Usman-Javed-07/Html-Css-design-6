9ac75187b2d1f5e22e5dc6ba89f4c4cf3107ff0b
Usman Javed <usmanjaved0816@gmail.com>
2024-10-09 12:30:30 +0500

    js object functinality

--- js/script.js
+++ js/script.js
@@ -14,28 +14,102 @@ if(pageName === "index.html") {
 //  add to card
 
 
-let removeCardItemButton = document.getElementsByClassName ('remove-Btn');
-console.log(removeCardItemButton);
-for (let i = 0; i < removeCardItemButton.length ; i++) {
-  let button = removeCardItemButton[i]
-  button.addEventListener ('click', function (){
-    let buttonclicked = event.target;
-    buttonclicked.parentElement.parentElement.remove();
-  });
-}
 
-
-let productData = [
+const productItems = [
   {
-      prodID: 19,
-      prodName: "Cartoon Shirt",
-      prodAdress: "img/products/f1.jpg",
-      prodBrand: "Adidas",
-      categories: ["men", "shirts", "featured"],
-      prodPrice: 78,
-  }
+        productID : 19,
+        productName : 'carton shirt',
+        productImage : 'images/card1.jpg',
+        productBrand : 'Adidas',
+        productPrice : 78,
+        // categories : ['men' , 'shirts', 'featured'],
+  },
+  {
+    productID : 37,
+    productName : 'Havain shirt',
+    productImage : 'images/card 2.jpg',
+    productBrand : 'Chanel',
+    productPrice : 111,
+    // categories : ['men' , 'shirts', 'featured'],
+},
+{
+  productID : 46,
+  productName : 'Printed shirt',
+  productImage : 'images/card3.jpg',
+  productBrand : 'Prada',
+  productPrice : 265,
+  // categories : ['men' , 'shirts', 'featured'],
+},
+{
+  productID : 55,
+  productName : 'Emo-Boy Shirt',
+  productImage : 'images/card4.jpg',
+  productBrand : 'GAP',
+  productPrice : 99,
+  // categories : ['men' , 'shirts', 'featured'],
+},
+{
+  productID : 307,
+  productName : 'off white shirt',
+  productImage : 'images/card5.jpg',
+  productBrand : 'H3h3',
+  productPrice : 76,
+  // categories : ['men' , 'shirts', 'featured'],
+},
+{
+  productID : 110,
+  productName : 'green Cargo Pants',
+  productImage : 'images/card6.jpg',
+  productBrand : 'Adidas',
+  productPrice : 290,
+  // categories : ['men' , 'pants', 'featured'],
+},
+{
+  productID : 115,
+  productName : ' blue Cargo Pants',
+  productImage : 'images/card7.jpg',
+  productBrand : 'H3h3',
+  productPrice : 210,
+  // categories : ['men' , 'pants', 'featured'],
+},
+{
+  productID : 130,
+  productName : 'Brown Cargo Pants',
+  productImage : 'images/card8.jpg',
+  productBrand : 'Adidas',
+  productPrice : 230,
+  // categories : ['men' , 'pants', 'featured'],
+},
+{
+  productID : 64,
+  productName : 'winter shirts',
+  productImage : 'images/f6.jpg',
+  productBrand : 'None',
+  productPrice : 8,
+  // categories : ['men' , 'shirts', 'featured'],
+},
+{
+  productID : 73,
+  productName : 'Pants',
+  productImage : 'images/f7.jpg',
+  productBrand : 'H3h3',
+  productPrice : 875,
+  // categories : ['women' , 'pants', 'featured'],
+},
+{
+  productID : 82,
+  productName : 'ladies shirts',
+  productImage : 'images/f8.jpg',
+  productBrand : 'H3h3',
+  productPrice : 82,
+  // categories : ['women' , 'shirts', 'featured'],
+},
 ]
 
-
-
-
+for (var i = 0 ; i < productItems.length; i++) {
+  debugger
+  console.log(productItems[i][productID]);
+  let k=`
+   
+  `
+}
