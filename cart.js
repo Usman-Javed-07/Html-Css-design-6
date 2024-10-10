84c8a7e2c9068fd720c2883a13b225a50f286ea9
Usman Javed <usmanjaved0816@gmail.com>
2024-10-10 14:16:34 +0500

    solving problems

--- cart.js
+++ cart.js
@@ -1,31 +1,36 @@
 
-console.log (' local storage data' , localStorage.getItem('product Data'));
+let currentData = JSON.parse(localStorage.getItem('product Data'));
 
 
 let cartData = `
-          <!-- <tr>
-            <td class="remove">
+
+ <tr>
+ <td class="remove">
               <button class="remove-Btn">
                 <i class="fa fa-times-circle"></i>
               </button>
             </td>
             <td class="imgAdress">
-             
+              <img src="${currentData[4].productImage}" alt="" />
             </td>
-            <td class="ID">${}</td>
-            <td class="name">${currProd.prodName}</td>
-            <td class="price">$${currProd.prodPrice}</td>
+            <td class="ID">${currentData[2].productID}</td>
+            <td class="name">${currentData[2].productName}</td>
+            <td class="price">$${currentData[2].productPrice}</td>
             <td>
               <input
                 type="number"
-                value="${quantity}"
+                value="$"
                 min="1"
                 class="quantity-input"
               />
             </td>
-            <td class="subtotal">$${subtotal}</td>
-          </tr> -->
+            <td class="subtotal">$</td>
+          </tr>
 
-          `
+`;
           const cartDisplay = document.querySelector('.section-p1')
           cartDisplay.innerHTML+=cartData;
+
+
+
+
